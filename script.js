708842983f971a8890cd7a95885ef81c4aa02e1f
“Holly <hollyefig@gmail.com>
2022-11-19 12:37:53 -0500

    reworked entire site. Made HTML and JS more efficient

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //grab calcGrid, mainNum display and secondNum display
 const calcGrid = document.querySelector(".calcGrid"),
   mainNum = document.querySelector(".mainNum"),
+  // grab formula display div
   secondNum = document.querySelector(".secondNum");
 
 // div for main number
@@ -8,78 +9,122 @@ const mainNumChild = document.createElement("div");
 mainNumChild.setAttribute("class", "mainNum");
 mainNum.appendChild(mainNumChild);
 
-let baseNum = [0];
-let prevNum;
-let expression;
+let num = [];
 let allNums = [];
+let expression;
+let formulaDisplay = [];
 let finalResult;
-mainNumChild.textContent = 0;
+mainNumChild.textContent = "";
 
-const numEntered = (e) => {
-  if (baseNum[0] === 0) {
-    baseNum.shift();
+const add = (e) => {
+  return e.reduce((a, b) => a + b, 0);
+};
+const subtract = (e) => {
+  return e.reduce((a, b) => a - b);
+};
+const multi = (e) => {
+  return e.reduce((a, b) => a * b);
+};
+const divide = (e) => {
+  return e.reduce((a, b) => a / b);
+};
+
+const operate = (e) => {
+  if (expression !== "equal") {
+    allNums.push(Number(num.join("")));
+  } else {
+    formulaDisplay = [finalResult];
   }
-  baseNum.push(e.value);
-  mainNumChild.textContent = baseNum.join("");
-};
 
-const eraseAll = () => {
-  baseNum = [0];
-  prevNum = 0;
-  expression = 0;
-  allNums = [];
-  finalResult = 0;
-  mainNumChild.textContent = 0;
-  secondNum.textContent = "";
-};
+  // if addition
+  if (expression === "add") {
+    if (finalResult === undefined && allNums.length === 2) {
+      finalResult = add(allNums);
+      mainNumChild.textContent = finalResult;
+      allNums = [];
+      allNums.push(finalResult);
+    } else if (allNums.length === 2) {
+      let result = add(allNums);
+      finalResult = result;
+      mainNumChild.textContent = finalResult;
+      allNums = [];
+      allNums.push(finalResult);
+    }
+  }
+  // if subtraction
+  if (expression === "subtract") {
+    if (finalResult === undefined && allNums.length === 2) {
+      finalResult = subtract(allNums);
+      mainNumChild.textContent = finalResult;
+      allNums = [];
+      allNums.push(finalResult);
+    } else if (allNums.length === 2) {
+      let result = subtract(allNums);
+      finalResult = result;
+      mainNumChild.textContent = finalResult;
+      allNums = [];
+      allNums.push(finalResult);
+    }
+  }
 
-const express = (e) => {
-  mainNumChild.textContent = "";
-  finalResult === undefined
-    ? (secondNum.textContent = baseNum.join(""))
-    : (secondNum.textContent = finalResult);
-  prevNum = Number(baseNum.join(""));
+  // if multiply
+  if (expression === "multiply") {
+    if (finalResult === undefined && allNums.length === 2) {
+      finalResult = multi(allNums);
+      mainNumChild.textContent = finalResult;
+      allNums = [];
+      allNums.push(finalResult);
+    } else if (allNums.length === 2) {
+      let result = multi(allNums);
+      finalResult = result;
+      mainNumChild.textContent = finalResult;
+      allNums = [];
+      allNums.push(finalResult);
+    }
+  }
+
+  // if divide
+  if (expression === "divide") {
+    if (finalResult === undefined && allNums.length === 2) {
+      finalResult = divide(allNums);
+      mainNumChild.textContent = finalResult;
+      allNums = [];
+      allNums.push(finalResult);
+      console.log("allNums after RESET", Number(allNums.join("")));
+    } else if (allNums.length === 2) {
+      let result = divide(allNums);
+      finalResult = result;
+      mainNumChild.textContent = finalResult;
+      allNums = [];
+      allNums.push(finalResult);
+    }
+  }
+
   if (e.id === "add") {
     expression = "add";
+    formulaDisplay.push("+");
   } else if (e.id === "subtract") {
     expression = "subtract";
+    formulaDisplay.push("-");
   } else if (e.id === "multiply") {
     expression = "multiply";
+    formulaDisplay.push("x");
   } else if (e.id === "divide") {
     expression = "divide";
+    formulaDisplay.push("÷");
+  } else if (e.id === "equal") {
+    expression = "equal";
+    mainNumChild.textContent = finalResult;
   }
-  baseNum = [];
+  secondNum.textContent = formulaDisplay.join("");
+  num = [];
+
+  console.log("current expression", expression);
 };
 
-const operate = () => {
-  mainNumChild.textContent = "";
-  secondNum.textContent = baseNum.join("");
-  if (expression === "add") {
-    let newNum = Number(baseNum.join(""));
-    finalResult === undefined ? null : (prevNum = finalResult);
-    let result = prevNum + newNum;
-    finalResult = result;
-  } else if (expression === "subtract") {
-    let newNum = Number(baseNum.join(""));
-    finalResult === undefined ? null : (prevNum = finalResult);
-    let result = prevNum - newNum;
-    finalResult = result;
-  } else if (expression === "multiply") {
-    let newNum = Number(baseNum.join(""));
-    finalResult === undefined ? null : (prevNum = finalResult);
-    let result = prevNum * newNum;
-    finalResult = result;
-  } else if (expression === "divide") {
-    let newNum = Number(baseNum.join(""));
-    finalResult === undefined ? null : (prevNum = finalResult);
-    let result = prevNum / newNum;
-    finalResult = result;
-  }
-  mainNumChild.textContent = finalResult;
-  baseNum = [];
-};
-
-const backspace = () => {
-  baseNum.pop();
-  mainNumChild.textContent = baseNum.join("");
+const numEntered = (e) => {
+  num.push(Number(e.innerText));
+  console.log("numArray", num);
+  formulaDisplay.push(e.innerText);
+  secondNum.textContent = formulaDisplay.join("");
 };
